ff755f2bfa0170fe91100255a8dcf89b45c79248
Yang Zhifei <xividyzf@gmail.com>
2014-07-09 23:45:08 +0800

    another error

--- EateryMaster/settings.py
+++ EateryMaster/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'CookeryMester',
+    'CookeryMaster',
 )
 
 MIDDLEWARE_CLASSES = (
